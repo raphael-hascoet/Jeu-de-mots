cc623ccba0fabf26bc6c3bcac9edec12c9a31739
lea <lea.louesdon@gmail.com>
2019-05-24 09:26:44 +0200

    Avancé

--- Client/src/app/jeu-solo/jeu-solo.component.ts
+++ Client/src/app/jeu-solo/jeu-solo.component.ts
@@ -1,17 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 @Component({
     selector: 'app-jeu-solo',
     templateUrl: './jeu-solo.component.html',
     styleUrls: ['./jeu-solo.component.css'],
-    host: { '(document:keypress)': 'handleKeyboardEvent($event)' },
 })
+const config: SocketIoConfig = { url: 'http://localhost:4444', options: {} };
+
 export class JeuSoloComponent implements OnInit {
+    socket: SocketIOClient.Socket;
+
     constructor() {}
 
     ngOnInit() {}
 
-    handleKeyboardEvent(event: KeyboardEvent) {
-        console.log(event);
+    @ViewChild('box') input: ElementRef;
+
+    value = '';
+    enterValue(value: string) {
+        this.value = value;
+        this.input.nativeElement.value = '';
     }
 }
