0642a1a784a309aee5f3787391769f46ed3a2d8f
Simon <simon.lebars.22290@gmail.com>
2019-05-24 11:02:34 +0200

    Commit qui sert à rien

--- server/index.ts
+++ server/index.ts
@@ -1,4 +1,5 @@
 import { Game } from './src/Game';
+import { calculateWordScore } from './src/gameUtils';
 
 const app = require('express')();
 var http = require('http').createServer(app);
@@ -6,6 +7,7 @@ var io = require('socket.io')(http);
 
 io.on('connection', function(socket: any) {
     console.log('a user connected');
+
     socket.on('startGame', function(gameConfig: Game) {
         console.log('Game started with config :');
         console.log(JSON.stringify(gameConfig));
@@ -14,4 +16,8 @@ io.on('connection', function(socket: any) {
 
 const server = http.listen(3000, () => {
     console.log('server is running on port', server.address().port);
+
+    console.log(calculateWordScore('boom', 'bimbamboom'));
+    console.log(calculateWordScore('bim', 'biim'));
+    console.log(calculateWordScore('', 'rien'));
 });
