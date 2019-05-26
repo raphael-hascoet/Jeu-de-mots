5720801ebbe317e8542068d94bd1505d19de0282
Simon Le Bars <simon.lebars.22290@gmail.com>
2019-05-26 16:35:35 +0200

    US 1.5 Démarrer une partie

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -9,15 +9,38 @@ export class AppComponent {
   title = 'Jeu de Mots';
   componentToDisplay:string = "game-config"; 
 
-  displayJeuSolo(): boolean{
-    return this.componentToDisplay=="jeu-solo";
+  /**
+   * Méthode indiquant d'afficher la vue de la partie
+   */
+  changeViewToGame():void{
+    this.componentToDisplay = "game";
   }
 
-  changerComposant():void{
-    this.componentToDisplay = "autre";
+  /**
+   * Méthode retournant un Booléan indiquant s'il faut afficher la vue de la partie
+   */
+  displayGame(): boolean{
+    return this.componentToDisplay=="game";
   }
 
+  /**
+   * Méthode indiquant d'afficher la vue de configuration de partie
+   */
+  changeViewToGameConfig():void{
+    this.componentToDisplay = "game-config";
+  }
+
+  /**
+   * Méthode retournant un Booléan indiquant s'il faut afficher la vue de configuration de la partie
+   */
   displayGameConfig() : boolean {
     return this.componentToDisplay == "game-config";
+  }
+
+  /**
+   * Méthode récupérent le composant app pour le transmettre aux autres vues
+   */
+  getAppComponent() : AppComponent{
+    return this;
   }
 }
--- Client/src/app/game-configuration-view/game-configuration-view.component.ts
+++ Client/src/app/game-configuration-view/game-configuration-view.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { GameService } from '../service/game.service';
 import { GameConfig } from "../model/game-config/game-config";
+import { AppComponent } from '../app.component';
 
 @Component({
   selector: 'app-game-configuration-view',
@@ -8,6 +9,9 @@ import { GameConfig } from "../model/game-config/game-config";
   styleUrls: ['./game-configuration-view.component.css']
 })
 export class GameConfigurationViewComponent implements OnInit {
+
+  @Input() parent: AppComponent;
+
   maxDifficulty : number;
   minDifficulty : number;
 
@@ -16,10 +20,17 @@ export class GameConfigurationViewComponent implements OnInit {
   ngOnInit() {
     this.gameService.getMinDifficulty().subscribe(value => this.minDifficulty=value);
     this.gameService.getMaxDifficulty().subscribe(value => this.maxDifficulty=value);
+
+    console.log(this.parent);
+
   }
 
   createGame(hostName : string, hostTeam : string, gameDifficulty : number) : void{
     this.gameService.createGame(new GameConfig(hostName, hostTeam, gameDifficulty));
+  }
+
+  changeViewToGame(){
+    this.parent.changeViewToGame();
   }
 
   getMinDifficulty(): number{
