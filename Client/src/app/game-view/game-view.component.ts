690a60eb0abb0a510ab0331a145900097fa7b60b
lea <lea.louesdon@gmail.com>
2019-05-27 20:23:46 +0200

    Win a game

--- Client/src/app/game-view/game-view.component.ts
+++ Client/src/app/game-view/game-view.component.ts
@@ -3,45 +3,48 @@ import { AppComponent } from '../app.component';
 import { SocketService } from '../service/socket.service';
 
 @Component({
-  selector: 'app-game-view',
-  templateUrl: './game-view.component.html',
-  styleUrls: ['./game-view.component.css']
+    selector: 'app-game-view',
+    templateUrl: './game-view.component.html',
+    styleUrls: ['./game-view.component.css'],
 })
 export class GameViewComponent implements OnInit {
+    @Input() parent: AppComponent;
 
-  @Input() parent : AppComponent;
+    title = 'app';
+    incomingmsg = [];
+    msg = 'First Protocol';
+    @ViewChild('box') input: ElementRef;
 
-  title = 'app';
-  incomingmsg = [];
-  msg = 'First Protocol';
-  @ViewChild('box') input: ElementRef;
+    @ViewChild('response') response: ElementRef;
+    constructor(private socketService: SocketService) {}
 
-  @ViewChild('response') response: ElementRef;
-  constructor(private socketService: SocketService) {}
+    ngOnInit() {
+        this.socketService.getScore().subscribe(msg => {
+            this.response.nativeElement.value =
+                msg[0] +
+                ' : ' +
+                msg[1] +
+                '\n' +
+                this.response.nativeElement.value;
+        });
 
-  ngOnInit() {
-      this.socketService.getScore().subscribe(msg => {
-          this.response.nativeElement.value =
-              msg[0] +
-              ' : ' +
-              msg[1] +
-              '\n' +
-              this.response.nativeElement.value;
-      });
-  }
+        this.socketService.hasWon().subscribe(msg => {
+            this.response.nativeElement.value =
+                'Gagné ! ' + '\n' + this.response.nativeElement.value;
+        });
+    }
 
-  sendProposition(proposition) {
-      this.socketService.sendProposition(proposition);
-  }
+    sendProposition(proposition) {
+        this.socketService.sendProposition(proposition);
+    }
 
-  value = '';
-  enterValue(value: string) {
-      this.value = value;
-      this.input.nativeElement.value = '';
-      //Check pour les string vide
-      if (value.replace(/\s/g, '').length != 0) {
-          this.sendProposition(value);
-      }
-  }
-
+    value = '';
+    enterValue(value: string) {
+        this.value = value;
+        this.input.nativeElement.value = '';
+        //Check pour les string vide
+        if (value.replace(/\s/g, '').length != 0) {
+            this.sendProposition(value);
+        }
+    }
 }
