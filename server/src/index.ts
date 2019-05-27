dfd378107cf674e1a7ade67f690fe119f7176622
lea <lea.louesdon@gmail.com>
2019-05-27 16:59:54 +0200

    Comptage de points

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import { Game } from '../src/Game';
 import { calculateWordScore } from '../src/gameUtils';
 import { Player } from '../src/Player';
-import { GameConfiguration } from "./GameConfiguration";
+import { GameConfiguration } from './GameConfiguration';
 
 const app = require('express')();
 var http = require('http').createServer(app);
@@ -12,16 +12,31 @@ gameConfiguration.calculLevelInterval();
 io.on('connection', function(socket: any) {
     console.log('a user connected');
 
-    socket.on('startGame', function(gameConfig: any) {
+    socket.on('startGame', async function(gameConfig: any) {
         console.log('Game started with config :');
         console.log(JSON.stringify(gameConfig));
 
-        Game.getInstance(new Player(gameConfig.hostName, gameConfig.hostTeam), gameConfig.gameDifficulty)
-        console.log("hostName : "+Game.getInstance().getHost().getName());
-        console.log("hostTeam : "+Game.getInstance().getHost().getTeam());
-        console.log("difficultyLevel : "+Game.getInstance().getDifficultyLevel());
-        
-        
+        Game.getInstance(
+            new Player(gameConfig.hostName, gameConfig.hostTeam),
+            gameConfig.gameDifficulty
+        );
+        await Game.getInstance().startGame();
+        console.log(
+            'hostName : ' +
+                Game.getInstance()
+                    .getHost()
+                    .getName()
+        );
+        console.log(
+            'hostTeam : ' +
+                Game.getInstance()
+                    .getHost()
+                    .getTeam()
+        );
+        console.log(
+            'difficultyLevel : ' + Game.getInstance().getDifficultyLevel()
+        );
+        console.log('Mot a trouver : ' + Game.getInstance().getWordToFind());
     });
     socket.on('proposition', function(msg: string) {
         console.log('Mot proposé :');
@@ -30,23 +45,23 @@ io.on('connection', function(socket: any) {
         socket.emit('score', [msg, score]);
     });
 
-    socket.on('getMinimalDifficulty', function(){
+    socket.on('getMinimalDifficulty', function() {
         gameConfiguration.calculLevelInterval();
-        socket.emit('minDifficulty', gameConfiguration.getMinimalDifficulty())
-    })
+        socket.emit('minDifficulty', gameConfiguration.getMinimalDifficulty());
+    });
 
-    socket.on('getMaximalDifficulty', function(){
+    socket.on('getMaximalDifficulty', function() {
         gameConfiguration.calculLevelInterval();
-        socket.emit('maxDifficulty', gameConfiguration.getMaximalDifficulty())
-    })
+        socket.emit('maxDifficulty', gameConfiguration.getMaximalDifficulty());
+    });
 });
 
 const server = http.listen(3000, async () => {
     console.log('server is running on port', server.address().port);
 
-    console.log("Resultat attendu 3 : " + calculateWordScore('boom', 'bimbamboom'));
-    console.log("Resultat attendu 3 : " + calculateWordScore('bim', 'biim'));
-    console.log("Resultat attendu 0 : " + calculateWordScore('', 'rien'));
-    await Game.getInstance().startGame();
-    console.log(Game.getInstance().getWordToFind());
+    console.log(
+        'Resultat attendu 3 : ' + calculateWordScore('boom', 'bimbamboom')
+    );
+    console.log('Resultat attendu 3 : ' + calculateWordScore('bim', 'biim'));
+    console.log('Resultat attendu 0 : ' + calculateWordScore('', 'rien'));
 });
