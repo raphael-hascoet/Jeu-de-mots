abe617e1a6bffeb0237b1f1a9be35738833c43a9
Zigotote <oce88b@gmail.com>
2019-05-31 12:05:17 +0200

    Problème mat-dialog-content

--- Client/src/app/game-command/game-command.component.ts
+++ Client/src/app/game-command/game-command.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { SocketService } from '../service/socket.service';
+import { MatDialog } from '@angular/material';
+import { GiveupDialogComponent } from './giveup-dialog/giveup-dialog.component';
 
 @Component({
     selector: 'app-game-command',
     templateUrl: './game-command.component.html',
     styleUrls: ['./game-command.component.css'],
 })
+/**
+ * Classe représentant les commandes disponibles durant le jeu
+ */
 export class GameCommandComponent implements OnInit {
-    constructor(private socketService: SocketService) {}
+    constructor(
+        private socketService: SocketService,
+        public giveUpDialog: MatDialog
+    ) {}
 
     ngOnInit() {
         this.socketService.getWords().subscribe(msg => {
@@ -26,7 +34,20 @@ export class GameCommandComponent implements OnInit {
         });
     }
 
+    /**
+     * Méthode permettant d'afficher les meilleurs mots proposés par les joueurs
+     */
     onShowWords() {
         this.socketService.askForWords();
+    }
+
+    /**
+     * Méthode permettant d'ouvrir une boîte de dialogue pour quitter la partie
+     */
+    onGiveUp() {
+        const dialogRef = this.giveUpDialog.open(GiveupDialogComponent);
+        dialogRef.afterClosed().subscribe(result => {
+            console.log('$result');
+        });
     }
 }
---
+++ Client/src/app/game-command/giveup-dialog/giveup-dialog.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-giveup-dialog',
+    templateUrl: './giveup-dialog.component.html',
+})
+/**
+ * Classe représentant la boite de dialogue d'abandon de partie
+ */
+export class GiveupDialogComponent {}
