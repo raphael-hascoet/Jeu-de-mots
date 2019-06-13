c6a1b849204ece26319fcd16fa36815c56c338dd
raphael-hascoet <39961449+raphael-hascoet@users.noreply.github.com>
2019-06-13 22:24:20 +0200

    Feature/server in env (#36)
    * Server environment in .env
    
    * Updated README and .gitignore
    
    * Test push
    
    * Test push
    
    * Test push
    
    * Test push
    
    * Test push
    
    * Test push
    
    * Added firebase to client
    
    * Updated .gitignore

---
+++ Client/src/environments/environment.hosting.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+    production: false,
+    serverUrl:
+        'https://jeu-de-mots-server-jeu-de-mots-server.apps.us-east-2.online-starter.openshift.com/',
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
